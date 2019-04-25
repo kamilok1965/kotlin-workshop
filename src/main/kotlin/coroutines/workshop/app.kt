7bf7de773d8b36a6465f0f4a59cc4bed18237632
Błażej Kardyś <bkardys@virtuslab.com>
2019-04-25 12:43:48 +0200

    Solution with breaks

--- src/main/kotlin/coroutines/workshop/app.kt
+++ src/main/kotlin/coroutines/workshop/app.kt
@@ -1,12 +1,83 @@
 package coroutines.workshop
 
-import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+import sun.plugin.dom.exception.InvalidStateException
+import kotlin.coroutines.CoroutineContext
 
+lateinit var api: Api
+lateinit var token: String
+val channelIn = Channel<String>(4)
+var channelOut = Channel<Pair<String, String>>(Channel.UNLIMITED)
+
+object OurContext : CoroutineScope {
+
+    val job: Job by lazy { SupervisorJob() }
+
+    override val coroutineContext: CoroutineContext
+        get() = job + Dispatchers.Default
+
+    fun fail() {
+        coroutineContext.cancelChildren()
+    }
+}
+
+@ExperimentalCoroutinesApi
 suspend fun main(): Unit = coroutineScope {
-    val api = Api(url = "http://localhost:9000") // remember to change `localhost` to proper url
+    api = Api(url = "https://kotlin-coroutines.herokuapp.com/") // remembe remember to change `localhost` to proper url
+    token = withContext(Dispatchers.Default) {
+        api.request(Register("Johann Georg Faust", "#pato"))
+    }.token
 
-    TODO()
+    for (a in 0..3) {
+        launch {
+            var encrypted = channelIn.receive()
+            var prepared: PasswordPrepared? = null
+            var decoded: PasswordDecoded? = null
+            while (true) {
+                val decrypter = Decrypter()
+                OurContext.launch {
+                    while (true) {
+                        try {
+                            if (!isActive) {
+                                break
+                            }
+                            prepared = prepared ?: decrypter.prepare(encrypted).takeIf { isActive }
+                                    ?: break
+                            decoded = decoded ?: decrypter.decode(prepared!!).takeIf { isActive }
+                                    ?: break
+                            val decrypted = decrypter.decrypt(decoded!!).takeIf { isActive } ?: break
+                            channelOut.send(Pair(encrypted, decrypted))
+                            prepared = null
+                            decoded = null
+                            encrypted = channelIn.receive()
+                        } catch (e: IllegalStateException) {
+                            OurContext.fail()
+                        } catch (e: java.net.UnknownHostException) {
+
+                        }
+                    }
+                }.join()
+            }
+        }
+
+    }
+    launch {
+        while (true) {
+            channelIn.send(api.request(PasswordRequest(token)).encryptedPassword)
+        }
+    }
+    launch {
+        while (true) {
+            try {
+                channelOut.receive().let { (encrypted, decrypted) ->
+                    api.request(Validate(token, encrypted, decrypted))
+                }
+            } catch (e: kotlinx.coroutines.channels.ClosedReceiveChannelException) {
+
+            }
+        }
+    }
 
     Unit
 }
-
