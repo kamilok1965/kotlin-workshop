79be9c6ffa4fdcf3bb485e4fe416b8a690cbe117
KrystianUjma <kujma@virtuslab.com>
2019-04-25 10:51:03 +0200

    add solution

--- src/main/kotlin/coroutines/workshop/app.kt
+++ src/main/kotlin/coroutines/workshop/app.kt
@@ -1,12 +1,50 @@
 package coroutines.workshop
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 
 suspend fun main(): Unit = coroutineScope {
-    val api = Api(url = "http://localhost:9000") // remember to change `localhost` to proper url
+    val api = Api(url = "https://kotlin-coroutines.herokuapp.com/")
+    val token = api.request(Register("Krystian", "KKUG Team")).token
+    val channelEncryptedPasswords = Channel<EncryptedPassword>(4)
+    val channelDecryptedPasswords = Channel<Validate>(20)
 
-    TODO()
+    launch(Dispatchers.IO) {
+        while (isActive) {
+            val encryptedPassword = api.request(PasswordRequest(token))
+            channelEncryptedPasswords.send(encryptedPassword)
+        }
+    }
 
-    Unit
+    launch(Dispatchers.IO) {
+        while (isActive) {
+            val validate = channelDecryptedPasswords.receive()
+            api.request(validate)
+        }
+    }
+
+    while (true) {
+        try {
+            coroutineScope {
+                (1..4).forEach {
+                    launch {
+                        val decrypter = Decrypter()
+                        while (isActive) {
+                            val encrypted = channelEncryptedPasswords.receive()
+                            val prepared = decrypter.prepare(encrypted.encryptedPassword)
+                            val decoded = decrypter.decode(prepared)
+                            val decrypted = decrypter.decrypt(decoded)
+                            channelDecryptedPasswords.send(Validate(token, encrypted.encryptedPassword, decrypted))
+                        }
+                    }.invokeOnCompletion { println(it) }
+                }
+            }
+        } catch (_: Throwable) {
+            println("catched!")
+        }
+    }
 }
 
