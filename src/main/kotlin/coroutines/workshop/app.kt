8ba651760333a6d0de634239da0dba483d907fe9
Paweł Marks <pmarks@virtuslab.com>
2019-04-26 16:30:26 +0200

    Simple solution using yielding

--- src/main/kotlin/coroutines/workshop/app.kt
+++ src/main/kotlin/coroutines/workshop/app.kt
@@ -1,12 +1,65 @@
 package coroutines.workshop
 
-import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
 
-suspend fun main(): Unit = coroutineScope {
-    val api = Api(url = "http://localhost:9000") // remember to change `localhost` to proper url
+suspend fun main() = supervisorScope {
 
-    TODO()
+    val api = Api(url = "http://kotlin-coroutines.herokuapp.com") // remember to change `localhost` to proper url
+    val register = Register("Kordyjan", "VirtusLab")
 
-    Unit
+
+    val token = api.request(register).token
+
+    val channelEncryptedPasswords = Channel<EncryptedPassword>()
+    val channelDecryptedPasswords = Channel<Validate>()
+
+
+
+    launch(Dispatchers.IO) {
+        while (isActive) {
+            val encryptedPassword = api.request(PasswordRequest(token))
+            channelEncryptedPasswords.send(encryptedPassword)
+        }
+    }
+
+
+
+
+
+
+    launch(Dispatchers.IO) {
+        while (isActive) {
+            val validate = channelDecryptedPasswords.receive()
+            api.request(validate)
+        }
+    }
+
+
+    while (true) {
+        try {
+            println("restarting")
+            coroutineScope {
+                (1..4).forEach { n ->
+                    launch {
+                        val decrypter = Decrypter()
+                        while (isActive) {
+                            val encryptedPassword = channelEncryptedPasswords.receive()
+                            yield()
+                            val prepared = decrypter.prepare(encryptedPassword.encryptedPassword)
+                            yield()
+                            val decoded = decrypter.decode(prepared)
+                            yield()
+                            val passwordDecrypted = decrypter.decrypt(decoded)
+                            yield()
+                            channelDecryptedPasswords.send(Validate(token, encryptedPassword.encryptedPassword, passwordDecrypted))
+
+                        }
+                    }.invokeOnCompletion { println("Worker #$n: $it") }
+                }
+            }
+        } catch (_: Throwable) {
+            println("Restart needed!")
+        }
+    }
 }
-
